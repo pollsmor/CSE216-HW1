d563aadc632a12b2b5e9c894511dc9c78cf371d9
Kevin Li <kevinli4321@gmail.com>
2021-03-03 17:22:27 -0500

    Part 1.2

--- hw1.ml
+++ hw1.ml
@@ -14,4 +14,10 @@ let rec float_pow x n =
   float_pow 1.5 2;; (* float: 2.25 *)
 
 (* 1.2 ============================================= *)
-  
+let rec compress lst =
+  match lst with
+  | [] -> []  (* Only run if list is empty, also for exhausting pattern match possibilities *)
+  | [n] -> [n]
+  | h::t -> if h = List.hd t then compress t else h::compress t;;
+
+compress ["a";"a";"b";"c";"c";"a";"a";"d";"e";"e";"e"];;
